17951e9577ff1fc70023da8c6c8b7d4cd7db7f8b
Gabriel F <78265172+G-F-D-L@users.noreply.github.com>
2026-02-12 15:52:05 -0300

    Refactor Passageiro class and update methods

--- POO part/Passageiro.java
+++ POO part/Passageiro.java
@@ -7,34 +7,19 @@ public class Passageiro {
     private int idPassageiro;
     private String nome;
     private int idade;
-    // Relacionamento (0, n): Passageiro compra vários bilhetes
-    private List<Bilhete> bilhetesComprados;
+    private List<Bilhete> bilhetesComprados = new ArrayList<>();
 
     public Passageiro(int idPassageiro, String nome, int idade) {
         this.idPassageiro = idPassageiro;
         this.nome = nome;
         this.idade = idade;
-        this.bilhetesComprados = new ArrayList<>();
     }
 
-    public void comprarBilhete(Bilhete bilhete) {
+    public void adicionarBilhete(Bilhete bilhete) {
         this.bilhetesComprados.add(bilhete);
-    }
-
-    // Getters
-    public int getIdPassageiro() {
-        return idPassageiro;
     }
 
     public String getNome() {
         return nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public List<Bilhete> getBilhetesComprados() {
-        return bilhetesComprados;
     }
 }
