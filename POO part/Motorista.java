e1ca462cbba975c7704439f395da1a02bdb81c01
Gabriel F <78265172+G-F-D-L@users.noreply.github.com>
2026-02-12 15:51:15 -0300

    Refactor Motorista class structure and initialization
    Removed unnecessary initialization of onibusConduzidos and adjusted formatting.

--- POO part/Motorista.java
+++ POO part/Motorista.java
@@ -6,29 +6,18 @@ public class Motorista {
 
     private int idMotorista;
     private String nome;
-    // Relacionamento (0, n): Motorista conduz vários ônibus
-    private List<Onibus> onibusConduzidos;
+    private List<Onibus> onibusConduzidos = new ArrayList<>();
 
     public Motorista(int idMotorista, String nome) {
         this.idMotorista = idMotorista;
         this.nome = nome;
-        this.onibusConduzidos = new ArrayList<>();
     }
 
     public void adicionarOnibus(Onibus onibus) {
         this.onibusConduzidos.add(onibus);
     }
 
-    // Getters e Setters
-    public int getIdMotorista() {
-        return idMotorista;
-    }
-
     public String getNome() {
         return nome;
-    }
-
-    public List<Onibus> getOnibusConduzidos() {
-        return onibusConduzidos;
     }
 }
