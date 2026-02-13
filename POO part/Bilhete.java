c11163e7c4372eef894e7b934e19476f727a0c8b
Daniel <danielgoncalves1485@gmail.com>
2026-02-13 10:36:35 -0300

    Update Bilhete.java

--- POO part/Bilhete.java
+++ POO part/Bilhete.java
@@ -1,29 +1,25 @@
-
-public class Bilhete {
+public class Bilhete{
 
     private int idBilhete;
     private double valor;
     private Viagem viagem;
     private Passageiro passageiro;
 
-    public Bilhete(int idBilhete, double valor, Viagem viagem, Passageiro passageiro) {
+    public Bilhete(int idBilhete, double valor, Viagem viagem, Passageiro passageiro){
         this.idBilhete = idBilhete;
         this.valor = valor;
         this.viagem = viagem;
         this.passageiro = passageiro;
 
-        // Vincula o bilhete ao passageiro e à viagem automaticamente
         passageiro.adicionarBilhete(this);
         viagem.registrarBilhete(this);
     }
 
-    // Método que busca o destino "emprestado" da viagem (evita redundância)
-    public String getDestino() {
+    public String getDestino(){
         return viagem.getDestino();
     }
 
-    @Override
-    public String toString() {
+    public String toString(){
         return "Bilhete #" + idBilhete + " | Destino: " + getDestino() + " | Valor: R$" + valor;
     }
 }
